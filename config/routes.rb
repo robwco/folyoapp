1e2457b83a063301a703a0481f9a13c12a58d533
Robert Williams <robwilliamsgd@gmail.com>
2016-02-24 10:16:57 -0800

    Add amazon S3 for paperclip uploads to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'site#homepage'
+  get 'welcome' => 'site#welcome'
   
 
   # The priority is based upon order of creation: first created -> highest priority.
